74ca8e66714dc22a942c33c190e6718dc69ffabe
optii <daniel.cole@etu.unilim.fr>
2016-08-08 15:54:25 +0200

    debug

--- Classes/GifCommand.php
+++ Classes/GifCommand.php
@@ -14,7 +14,7 @@ class GifCommand extends PoolbotBaseCommand
     protected function execute($message, $context) {
         if($this->pool->isRegistered($this->getCurrentUser()) && $this->pool->isAdmin($this->getCurrentUser())){
           $this->pool->toggleGif();
-          $this->send($this->getCurrentChannel(), null, ($this->pool->isGif()) ? "Gif mode activated" : "Gif mode deactivated")
+          $this->send($this->getCurrentChannel(), null, ($this->pool->isGif()) ? "Gif mode activated" : "Gif mode deactivated");
         } else {
             $this->send($this->getCurrentChannel(), null, 'You must be an admin');
         }
